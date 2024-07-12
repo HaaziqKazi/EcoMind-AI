53959efb6fb91b31212cb0c3984bffaa51ba77f6
HaaziqKazi <kazihaaziq@gmail.com>
2024-07-12 18:41:18 +0400

    keywords update

--- EcoMind/keyword_extraction.py
+++ EcoMind/keyword_extraction.py
@@ -3,17 +3,16 @@ from rake_nltk import Rake
 # Initialize the RAKE object
 rake_nltk_var = Rake()
 
-# Sample text for keyword extraction
-text = """
-what is the impact of climate change on pollinators
-"""
+def extract_keywords(query):
 
-# Extract keywords by passing the text
-rake_nltk_var.extract_keywords_from_text(text)
+    # Extract keywords by passing the text
+    rake_nltk_var.extract_keywords_from_text(query)
 
-# Get the ranked phrases with their scores
-keyword_extracted = rake_nltk_var.get_ranked_phrases_with_scores()
+    # Get the ranked phrases with their scores
+    keywords_extracted = rake_nltk_var.get_ranked_phrases_with_scores()
 
-# Print the keywords and their scores
-for score, keyword in keyword_extracted:
-    print(f"{keyword}: {score}")
+    # Print the keywords and their scores
+    for score, keyword in keywords_extracted:
+        print(f"{keyword}: {score}")
+
+    return keywords_extracted
--- EcoMind/main.py
+++ EcoMind/main.py
@@ -1,3 +1,5 @@
 import vectara
 
+query = input('what is your query')
+
 vectara()
