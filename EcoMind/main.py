175ce7eb974a6cfc9d5f5ae3ad634c61ae684259
HaaziqKazi <kazihaaziq@gmail.com>
2024-07-12 18:20:22 +0400

    added vectara support

---
+++ EcoMind/main.py
@@ -0,0 +1,3 @@
+import vectara
+
+vectara()
---
+++ EcoMind/vectara.py
@@ -0,0 +1,50 @@
+import requests
+import json
+
+def vectara(api_key, files):
+    url = "https://api.vectara.io/v2/corpora/new/upload_file"
+
+    payload={}
+
+    # don't change the first part that says file for the below
+    # can only put one file at a time
+    files = [
+      ('file', ('vectara_handbook', open('ADD FILE HERE', 'rb'), 'application/octet-stream'))
+    ]
+    headers = {
+      'Accept': 'application/json',
+      'x-api-key': 'zut_63JHduOA7XZXlewPHuQOD7YuamOctBXBaAeuQA'
+    }
+
+    response_upload = requests.request("POST", url, headers=headers, data=payload, files=files)
+
+    print(response_upload.text)
+
+
+    url = "https://api.vectara.io/v2/query"
+
+    payload = json.dumps({
+      "query": "Am I allowed to bring pets to work?",
+      "search": {
+        "corpora": [
+          {
+            "custom_dimensions": {},
+            "lexical_interpolation": 0,
+            "semantics": "default",
+            "corpus_key": "new"
+          }
+        ]
+        },
+      "stream_response": False
+    })
+    headers = {
+      'Content-Type': 'application/json',
+      'Accept': 'application/json',
+      'x-api-key': 'zut_63JHduOA7XZXlewPHuQOD7YuamOctBXBaAeuQA'
+    }
+
+    response = requests.request("POST", url, headers=headers, data=payload)
+
+    print(response.text)
+
+    return response.text
